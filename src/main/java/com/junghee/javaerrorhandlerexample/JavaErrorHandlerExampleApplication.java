2c6c861f5b909dd10a5e81baa5b759c8ff36f03f
junghee <junghee0130@lineus.kr>
2024-10-15 15:09:14 +0900

    settings

---
+++ src/main/java/com/junghee/javaerrorhandlerexample/JavaErrorHandlerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.junghee.javaerrorhandlerexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaErrorHandlerExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaErrorHandlerExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/junghee/javaerrorhandlerexample/JavaErrorHandlerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junghee.javaerrorhandlerexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaErrorHandlerExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
