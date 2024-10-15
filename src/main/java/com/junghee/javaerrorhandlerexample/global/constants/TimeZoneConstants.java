b75303345b1969c6d7635270207ec581fb323fd8
junghee <junghee0130@lineus.kr>
2024-10-15 15:16:13 +0900

    zoneDateTime id 설정

---
+++ src/main/java/com/junghee/javaerrorhandlerexample/global/constants/TimeZoneConstants.java
@@ -0,0 +1,10 @@
+package com.junghee.javaerrorhandlerexample.global.constants;
+
+import java.time.ZoneId;
+
+public class TimeZoneConstants {
+
+    public static final ZoneId UTC = ZoneId.of("UTC");
+    public static final ZoneId KST = ZoneId.of("Asia/Seoul");
+
+}
